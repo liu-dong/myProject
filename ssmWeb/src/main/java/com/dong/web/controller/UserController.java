7e298ef6d59be87ee8700568af3f1aa6f542db17
Dong <964708273@qq.com>
2019-03-25 23:18:19 +0800

    完成ssm框架的搭建与测试

--- ssmWeb/src/main/java/com/dong/web/controller/UserController.java
+++ ssmWeb/src/main/java/com/dong/web/controller/UserController.java
@@ -1,20 +1,31 @@
 package com.dong.web.controller;
 
 import com.dong.web.model.User;
+import com.dong.web.service.IUserService;
+import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
 
     private static Logger log= LoggerFactory.getLogger(UserController.class);
+
+    @Autowired
+    private IUserService userService;
 
     @RequestMapping(value="/test",method= RequestMethod.GET)
     public String test(HttpServletRequest request, Model model){
@@ -32,5 +43,62 @@ public class UserController {
         log.debug(user.toString());
         model.addAttribute("user", user);
         return "home";
+    }
+
+    @RequestMapping(value="/showUser",method=RequestMethod.GET)
+    public String toIndex(HttpServletRequest request,Model model){
+        int userId = Integer.parseInt(request.getParameter("id"));
+        System.out.println("userId:"+userId);
+        User user = this.userService.getUserById(userId);
+        log.debug(user.toString());
+        model.addAttribute("user", user);
+        return "showUser";
+    }
+
+    // /user/showUser3/{id}
+    @RequestMapping(value="/showUser3/{id}",method=RequestMethod.GET)
+    public String toIndex3(@PathVariable("id")String id, Map<String, Object> model){
+        int userId = Integer.parseInt(id);
+        System.out.println("userId:"+userId);
+        User user = this.userService.getUserById(userId);
+        log.debug(user.toString());
+        model.put("user", user);
+        return "showUser";
+    }
+
+    // /user/{id}
+    @RequestMapping(value="/{id}",method=RequestMethod.GET)
+    @ResponseBody
+    public User getUserInJson(@PathVariable String id, Map<String, Object> model){
+        int userId = Integer.parseInt(id);
+        System.out.println("userId:"+userId);
+        User user = this.userService.getUserById(userId);
+        log.info(user.toString());
+        return user;
+    }
+
+    // /user/{id}
+    @RequestMapping(value="/jsonType/{id}",method=RequestMethod.GET)
+    public ResponseEntity<User> getUserInJson2(@PathVariable String id, Map<String, Object> model){
+        int userId = Integer.parseInt(id);
+        System.out.println("userId:"+userId);
+        User user = this.userService.getUserById(userId);
+        log.info(user.toString());
+        return new ResponseEntity<User>(user, HttpStatus.OK);
+    }
+
+    //文件上传、
+    @RequestMapping(value="/upload")
+    public String showUploadPage(){
+        return "upload/file";
+    }
+
+    @RequestMapping(value="/doUpload",method=RequestMethod.POST)
+    public String doUploadFile(@RequestParam("file") MultipartFile file) throws IOException {
+        if (!file.isEmpty()) {
+            log.info("Process file:{}",file.getOriginalFilename());
+        }
+        FileUtils.copyInputStreamToFile(file.getInputStream(), new File("E:\\fileUpload\\",System.currentTimeMillis()+file.getOriginalFilename()));
+        return "success";
     }
 }
