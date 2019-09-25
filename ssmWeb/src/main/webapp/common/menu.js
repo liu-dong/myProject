d3f25f9d345b762e12bf49ea34011af70e0df172
liu-dong <964708273@qq.com>
2019-09-25 16:50:43 +0800

    添加菜单配置

---
+++ ssmWeb/src/main/webapp/common/menu.js
@@ -0,0 +1,46 @@
+var MENU_CONST = [
+    {
+        "menuName": "人员管理",
+        "url": "#",
+        "childMenu": [
+            {
+                "menuName": "人员信息列表",
+                "url": "/page/personManagement/personInfoList.html"
+            },
+            {
+                "menuName": "人员信息统计列表",
+                "url": "#"
+            }
+        ]
+    },
+    {
+        "menuName": "角色管理",
+        "url": "#",
+        "childMenu": [
+            {
+                "menuName": "角色信息列表",
+                "url": "#"
+            }
+        ]
+    },
+    {
+        "menuName": "权限管理",
+        "url": "#",
+        "childMenu": [
+            {
+                "menuName": "权限信息列表",
+                "url": "#"
+            }
+        ]
+    },
+    {
+        "menuName": "菜单管理",
+        "url": "#",
+        "childMenu": [
+            {
+                "menuName": "菜单信息列表",
+                "url": "#"
+            }
+        ]
+    }
+];
